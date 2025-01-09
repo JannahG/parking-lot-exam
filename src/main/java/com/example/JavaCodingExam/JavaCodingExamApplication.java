081fc00a6445e34fd1f942ddbd8cdb78cdee8014
jannah.ortiz.98 <jannahortiz@gmail.com>
2025-01-09 18:07:40 +0800

    Setup project base

---
+++ src/main/java/com/example/JavaCodingExam/JavaCodingExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaCodingExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaCodingExam/JavaCodingExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaCodingExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
